48c67ca2b7f0f0f7b09af7e81256ddce9769e298
Santiago Barrios <santiagobarriosgrijalba.08@gmail.com>
2020-03-23 00:14:13 -0500

    animaciones

--- jquery-to-js-curso-854bf9298991f38fb65a7e62694e0073fffed5c4/src/js/home.js
+++ jquery-to-js-curso-854bf9298991f38fb65a7e62694e0073fffed5c4/src/js/home.js
@@ -123,20 +123,18 @@ fetch('https://swapi.co/api/people/')
     
 })
 
-  const {data: {movies: actionList}} = await getData(`${BASE_API}list_movies.json?genre=action`);
-  const {data: {movies: terrorList}} = await getData(`${BASE_API}list_movies.json?genre=horror`);
-  const {data: {movies: animationList}} = await getData(`${BASE_API}list_movies.json?genre=animation`);
-  console.log(actionList, terrorList, animationList);
-  function videoItemTemplate(movie, category){
-    return (
-      `<div class="primaryPlaylistItem" data-id="${movie.id}" data-category= "${category}" >
-          <div class="primaryPlaylistItem-image">
-            <img src="${movie.medium_cover_image}">
-          </div>
-          <h4 class="primaryPlaylistItem-title">
-            ${movie.title}
-          </h4>
-        </div>`
+
+
+function videoItemTemplate(movie, category){
+  return (
+    `<div class="primaryPlaylistItem" data-id="${movie.id}" data-category= "${category}" >
+    <div class="primaryPlaylistItem-image">
+    <img src="${movie.medium_cover_image}">
+    </div>
+    <h4 class="primaryPlaylistItem-title">
+    ${movie.title}
+    </h4>
+    </div>`
     )
   }
   function createTemplate(HTMLString){
@@ -157,16 +155,23 @@ fetch('https://swapi.co/api/people/')
       const HTMLString = videoItemTemplate(movie, category);
       const movieElement = createTemplate(HTMLString);
       $container.append(movieElement);
+      const image = movieElement.querySelector('img');
+      image.addEventListener('load', (event)=>{
+        event.srcElement.classList.add('fadeIn');
+      })
       addEventClick(movieElement);
     })
   }
   
+  const {data: {movies: actionList}} = await getData(`${BASE_API}list_movies.json?genre=action`);
   const $actionContainer = document.querySelector('#action');
   renderMovieList(actionList, $actionContainer, 'action');
-
+  
+  const {data: {movies: terrorList}} = await getData(`${BASE_API}list_movies.json?genre=horror`);
   const $dramaContainer = document.getElementById('drama');
   renderMovieList(terrorList, $dramaContainer, 'drama' );
-
+  
+  const {data: {movies: animationList}} = await getData(`${BASE_API}list_movies.json?genre=animation`);
   const $animationContainer = document.getElementById('animation'); 
   renderMovieList(animationList, $animationContainer, 'animation' );
 
